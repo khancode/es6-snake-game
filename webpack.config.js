dff7fd44236fef1dcbbc34f7afb44e76ea9c7ff0
khancode <khancodegt@gmail.com>
2018-03-21 21:38:40 -0400

    added webpack-dev-server that provides live reloading for fast dev work

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,11 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
   }
 };
